dee67a6cd46550f0784ad9c9dddb87ba03dc5dda
Bhanu Prasad Polam <44427511+pbhanu999@users.noreply.github.com>
2019-07-15 00:46:41 +0530

    Create regexp.sql

---
+++ regexp.sql
@@ -0,0 +1,21 @@
+SELECT * FROM employees.employees
+where last_name regexp 'bam'
+
+-- where last_name regexp '^bam'
+
+-- where last_name regexp 'bam$'
+
+-- where last_name regexp '[a-h]s'
+
+-- ^ beginning
+-- $ ending of the string
+
+
+# emp_no, birth_date, first_name, last_name, gender, hire_date
+10003, 1959-12-03, Parto, Bamford, M, 1986-08-28
+13151, 1963-06-15, Sadun, Bamford, M, 1992-11-30
+15979, 1959-06-01, Prodip, Bamford, M, 1990-10-26
+16799, 1953-12-27, Shigeichiro, Bamford, M, 1986-04-26
+17553, 1962-12-02, Toshimitsu, Bamford, F, 1987-02-08
+18580, 1955-12-09, Mabo, Bamford, M, 1985-12-08
+21077, 1956-09-21, Siddarth, Bamford, F, 1995-08-31
