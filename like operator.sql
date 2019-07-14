3624da974d84f29378f3fa4376263ddf6403a9b4
Bhanu Prasad Polam <44427511+pbhanu999@users.noreply.github.com>
2019-07-15 00:26:35 +0530

    Create like operator.sql

---
+++ like operator.sql
@@ -0,0 +1,14 @@
+SELECT * FROM employees.employees
+where last_name like 'b%'
+
+
+# emp_no, birth_date, first_name, last_name, gender, hire_date
+10003, 1959-12-03, Parto, Bamford, M, 1986-08-28
+10012, 1960-10-04, Patricio, Bridgland, M, 1992-12-18
+10017, 1958-07-06, Cristinel, Bouloucos, F, 1993-08-03
+10026, 1953-04-03, Yongqiao, Berztiss, M, 1995-03-20
+10039, 1959-10-01, Alejandro, Brender, M, 1988-01-19
+10056, 1961-09-01, Brendon, Bernini, F, 1990-02-01
+10060, 1961-10-15, Breannda, Billingsley, M, 1987-11-02
+10068, 1962-11-26, Charlene, Brattka, M, 1987-08-07
+10069, 1960-09-06, Margareta, Bierman, F, 1989-11-05
